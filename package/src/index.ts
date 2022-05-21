41d987564910b66b8660ee797a8b8177c5f29390
josh-merrell <27826752+josh-merrell@users.noreply.github.com>
2022-05-21 13:56:18 -0600

    Add webpack and other base config files (#3)
    * test adding change and pushing to dev
    
    * adding .gitignore for Host App, with DEV-SETUP instructions. Also updated name of src/ to testing/. Within package/, added tsconfig and webpack.config, and .gitignore for node_modules. Added dist/ dir to package/ for future webpack implementation, currently our Host App is pointing straight to our src/ dir rather than our dist/ dir, so webpack is not implemented yet
    
    * re-committing prior commit with co-authors added - also added notes in DEV-SETUP.txt
    
    * Co-authored-by: Max N  90889459+Nattachai12@users.noreply.github.com
    Co-authored-by: Jerald Stephenson 62264085+JeraldStephenson@users.noreply.github.com

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,13 +1,13 @@
 // import { Request, Response } from 'express'
-import { renderHTML } from './renderHTML'
+import { renderHTML } from './renderHTML';
 
-const express = (options) => {
-    return (req, res, next) => {
-      res.setHeader('Content-Type', 'text/html')
-      const html = renderHTML(options)
-      res.write(html)
-      res.end()
-    }
-}
+const invokeGraphERRQL = (options) => {
+  return (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    const html = renderHTML(options);
+    res.write(html);
+    res.end();
+  };
+};
 
-export default express;
+export default invokeGraphERRQL;
