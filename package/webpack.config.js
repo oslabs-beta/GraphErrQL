ed3ae2c1fe164ce9b50f9ccf1ba6f92438c319aa
Jerald Stephenson <jeraldstephenson@Jeralds-MacBook-Pro.local>
2022-05-25 19:16:22 -0700

    added rules to webpack.config

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { resolve } = require('path');
 
 module.exports = {
     mode: 'production',
@@ -15,7 +16,24 @@ module.exports = {
         globalObject: 'this', //To make the library build available on both browsers and Node.js.  in case of a web browser 'this' will be the object window.
         umdNamedDefine: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['ts-loader']
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.ts', '.tsx']
+    },
     plugins: [
         new CleanWebpackPlugin(),
-    ],
+    ], 
 }
