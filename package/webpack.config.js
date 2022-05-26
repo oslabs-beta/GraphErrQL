0a4c72e839011b0b244669f6d51764469732f682
Jerald Stephenson <jeraldstephenson@Jeralds-MacBook-Pro.local>
2022-05-25 19:19:18 -0700

    installed babel loader and ts loaders

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -3,37 +3,35 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = {
-    mode: 'production',
-    target: 'web',
-    entry: {
-        index: './src/index.ts',  
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index.d.ts',
-        library: 'grapherrqlLibrary',
-        libraryTarget: 'umd',  //universal export, will allow for reference to dist script as var, amd, or common.js syntax
-        globalObject: 'this', //To make the library build available on both browsers and Node.js.  in case of a web browser 'this' will be the object window.
-        umdNamedDefine: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ['ts-loader']
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.jsx', '.js', '.ts', '.tsx']
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-    ], 
-}
+  mode: 'production',
+  target: 'web',
+  entry: {
+    index: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.d.ts',
+    library: 'grapherrqlLibrary',
+    libraryTarget: 'umd', //universal export, will allow for reference to dist script as var, amd, or common.js syntax
+    globalObject: 'this', //To make the library build available on both browsers and Node.js.  in case of a web browser 'this' will be the object window.
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.ts', '.tsx'],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
