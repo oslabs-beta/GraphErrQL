a6b1fd6dbb3d3d23e224fad4290933c97f8784ed
josh-merrell <27826752+josh-merrell@users.noreply.github.com>
2022-07-02 15:14:26 -0600

    Resolved issues incorporating MVP FE with Creat-React-App and react-scripts. Our grapherrql App is now rendering appropriately as a built package referenced by local Host-App server
    Co-authored-by: JeraldStephenson 62264085+JeraldStephenson@users.noreply.github.com
    Co-authored-by: GwenPhillips 94320086+gkphillips@users.noreply.github.com
    Co-authored-by: MariahTalicruan 76408932+MariahTali@users.noreply.github.com

--- grapherrql-package/src/App.tsx
+++ grapherrql-package/src/App.tsx
@@ -1,11 +1,38 @@
+// import React from 'react';
+// import './App.css';
+// import Header from './components/Header';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+        <Header />
+      </header>
     </div>
   );
 }
--- grapherrql-package/src/index.tsx
+++ grapherrql-package/src/index.tsx
@@ -6,7 +6,9 @@ import App from './App';
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
 // );
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
