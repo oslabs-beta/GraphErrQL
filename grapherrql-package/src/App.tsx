1deaf8b6950585492c7dde08dac9617d791c2b78
josh-merrell <27826752+josh-merrell@users.noreply.github.com>
2022-07-02 15:17:02 -0600

    minor fix to App.tsx

--- grapherrql-package/src/App.tsx
+++ grapherrql-package/src/App.tsx
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import './App.css';
-// import Header from './components/Header';
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
@@ -19,20 +5,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <Header />
-      </header>
+      <Header />
     </div>
   );
 }
