5d9016d970e7b32db64f97d1e19e1dbfdba7dc16
MariahTali <talicuran.mariah@gmail.com>
2022-07-05 19:21:37 -0700

    downgraded react react dom version

--- grapherrql-package/src/index.tsx
+++ grapherrql-package/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
 // );
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
