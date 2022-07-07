d46850a4fc0bb79f52c780f5f1f62d70a2a2b620
Gwen Phillips <gwen.phil@gmail.com>
2022-07-06 22:07:36 -0400

    merged TestGraphErrQL code with current repo
    Co-authored-by: Mariah 76408932+MariahTali@users.noreply.github.com

--- grapherrql-package/src/App.tsx
+++ grapherrql-package/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { GraphContextProvider } from './components/Context';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
+    <div>
+      <GraphContextProvider>
+        <Header />
+      </GraphContextProvider>
     </div>
   );
 }
---
+++ grapherrql-package/src/components/styles/Display.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const DisplaySandbox = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+`;
+
+export const QuerySandbox = styled.div`
+  width: 50%;
+  margin 2px;
+`;
+
+export const ResponseSandbox = styled.div`
+  width: 50%;
+  margin 2px;
+`;
