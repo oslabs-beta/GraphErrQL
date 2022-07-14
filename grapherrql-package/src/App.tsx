a7cd3454b8a5eb96e988a729c634dbf73978e95e
Gwen Phillips <gwen.phil@gmail.com>
2022-07-13 21:19:32 -0400

    folders reorganized

--- grapherrql-package/src/App.tsx
+++ grapherrql-package/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import { GraphContextProvider } from './components/Context';
+import { GraphContextProvider } from './components/SandboxMode/Context';
 
 function App() {
   return (
