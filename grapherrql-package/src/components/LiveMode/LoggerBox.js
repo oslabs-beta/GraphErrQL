38426a66059d48d12d91da9f866216896e8305d3
Jerald Stephenson <jeraldstephenson@Jeralds-MacBook-Pro.local>
2022-07-27 15:24:05 -0700

    wrote in logic to group query and response in containers, but keep them spaced enough within containers to separate them in a readable state

--- grapherrql-package/src/components/LiveMode/LoggerBox.js
+++ grapherrql-package/src/components/LiveMode/LoggerBox.js
@@ -43,14 +43,37 @@ function LoggerBox() {
   //     errorList.push(<ErrorItem key={i}>{JSON.stringify(error)}</ErrorItem>);
   //   });
   // }
+  const groupQueryResponse = (arr) => {
+    const result = [];
+    let i = 0;
+    while (i < arr.length) {
+      const innerArray = [];
+      innerArray.push(arr[i]);
+      ++i;
+      innerArray.push(arr[i]);
+      result.push(innerArray);
+      i++;
+    }
+    return result;
+  };
 
-  const displayDataLog = dataLog.map((item) => {
+  const groupedQueryResponses = groupQueryResponse(dataLog);
+
+  const displayDataLog = groupedQueryResponses.map((qR) => {
+    const queryResponse = qR.map((item) => {
+      return (
+        <>
+          <>{item}</> {/*should be a styled query container, onClick will trigger if response will show or hide */}
+          <div>**************{/*should be a styled response container with state for show: true or false */}</div>
+        </>
+      );
+    });
     return (
       <>
         {/* can wrap each item in styled component  */}
         <div></div>
-        {item}
-        <div>*************</div>
+        {queryResponse}
+        <div>-----------------</div>
       </>
     );
   });
