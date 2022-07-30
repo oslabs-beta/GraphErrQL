48d0709d498df1f710a7e103b5125808cdb47f08
Jerald Stephenson <jeraldstephenson@Jeralds-MacBook-Pro.local>
2022-07-29 17:01:24 -0700

    moved styled components from LoggerBox component into LoggerResponse.styled.js, imported the styled components into LoggerBox from LoggerResponse.stled.js, cleaned up comments and unused code.

--- grapherrql-package/src/components/LiveMode/styles/LoggerResponse.styled.js
+++ grapherrql-package/src/components/LiveMode/styles/LoggerResponse.styled.js
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 
-export const StyledErrorNoResponse = styled.pre`
-  background-color: #e9eef0;
-  border: 7px solid #ff1616;
-  color: black;
-  border-radius: 20px;
-  padding: 15px 15px;
-  font-size: 1.25em;
-`;
+// export const StyledErrorNoResponse = styled.pre`
+//   background-color: #e9eef0;
+//   border: 7px solid #ff1616;
+//   color: black;
+//   border-radius: 20px;
+//   padding: 15px 15px;
+//   font-size: 1.25em;
+// `;
 
-export const StyledErrorWithResponse = styled.pre`
-  background-color: #e9eef0;
-  border: 7px solid #ff1616;
-  color: black;
-  border-radius: 20px 20px 0px 0px;
-  padding: 15px 15px;
-  margin-bottom: 0px;
-  font-size: 1.25em;
-`;
+// export const StyledErrorWithResponse = styled.pre`
+//   background-color: #e9eef0;
+//   border: 7px solid #ff1616;
+//   color: black;
+//   border-radius: 20px 20px 0px 0px;
+//   padding: 15px 15px;
+//   margin-bottom: 0px;
+//   font-size: 1.25em;
+// `;
 
 export const StyledSuccessNoResponse = styled.div`
   display: flex;
@@ -30,13 +30,47 @@ export const StyledSuccessNoResponse = styled.div`
   border-radius: 20px;
   padding: 15px 15px;
 `;
+export const DataContainer = styled.div`
+  display: grid;
+`;
 
-export const StyledSuccessWithResponse = styled.pre`
+export const IncomingDataContainer = styled.div`
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1em;
+`;
+
+export const TextContainer = styled.div`
+  border-radius: 0.3rem;
+`;
+
+export const ErrorTextContainer = styled.div`
+  background-color: #e9eef0;
+  border: 7px solid #ff1616;
+  color: black;
+  border-radius: 20px;
+  padding: 10px;
+  font-size: 1.15em;
+`;
+
+export const CurrentQueryResponse = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 87.25vw;
   background-color: #e9eef0;
   border: 7px solid #078aa8;
   color: black;
-  border-radius: 20px 20px 0px 0px;
+  border-radius: 20px;
   padding: 15px 15px;
-  margin-bottom: 0px;
-  font-size: 1.25em;
 `;
+
+// export const StyledSuccessWithResponse = styled.pre`
+//   background-color: #e9eef0;
+//   border: 7px solid #078aa8;
+//   color: black;
+//   border-radius: 20px 20px 0px 0px;
+//   padding: 15px 15px;
+//   margin-bottom: 0px;
+//   font-size: 1.25em;
+// `;
