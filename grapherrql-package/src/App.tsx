03cf4c81fe68eb029bb27d89438b50c75b67ebba
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2022-08-01 17:44:14 -0700

    deleted mock data and loggerresponse (no longer used component), add a function to filter out text included from SSE in our query and responses

--- grapherrql-package/src/App.tsx
+++ grapherrql-package/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import { GraphContextProvider } from './components/SandboxMode/Context';
