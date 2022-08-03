6d3f90cb9de547fee6e069c3324e9354f9c6689d
josh-merrell <27826752+josh-merrell@users.noreply.github.com>
2022-08-02 21:13:59 -0600

    Added blinking Live Mode icon and removed unreferenced/missing icons in Manifest

--- grapherrql-package/src/components/LiveMode/styles/HeaderItemContainer.styled.js
+++ grapherrql-package/src/components/LiveMode/styles/HeaderItemContainer.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledHeaderItemContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background-color: #003845;
 `;
