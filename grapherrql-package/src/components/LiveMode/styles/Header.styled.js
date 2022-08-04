62f57acbc268742c47c111581ffa30a663a80391
Mariah Talicuran <76408932+MariahTali@users.noreply.github.com>
2022-08-04 13:06:14 -0700

    overhauled header pic logo and added gamey on brand features (#39)

--- grapherrql-package/src/components/LiveMode/styles/Header.styled.js
+++ grapherrql-package/src/components/LiveMode/styles/Header.styled.js
@@ -3,18 +3,26 @@ import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
   display: flex;
+  justify-content: space-between;
   font-family: 'Gagalin', sans-serif;
   font-size: large;
   color: white;
   background-color: #003845;
   padding: 15px 15px 15px 15px;
-  justify-content: space-evenly;
+  /* justify-content: space-evenly; */
   border: 7px solid #078aa8;
 `;
 
 export const Logo = styled.img`
-
-  object-fit: contain;
-  height: 200px;
-  margin-right: 10px;
+display: flex;
+object-fit: contain;
+height: 200px;
+/* margin-right: 10px; */
 `;
+
+export const HeaderRight = styled.img`
+display: flex;
+object-fit: contain;
+height: 200px;
+
+`
